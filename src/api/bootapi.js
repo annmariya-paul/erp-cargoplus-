a882dd87616305bcfdeb95a6c25b84d18d93c239
alansaji <alan@reontel.com>
2022-10-12 16:08:41 +0530

    component inheritance

--- src/api/bootapi.js
+++ src/api/bootapi.js
@@ -1,4 +1,4 @@
-const ApiSettings = require("../api_settings.config.json");
+const ApiSettings = require("../version_settings.config.json");
 const CRM_PREFIX = "crm";
 const CRM_BASE_URL = `${process.env.REACT_APP_BASE_URL}/${CRM_PREFIX}/${ApiSettings.crm.version_name}`;
 

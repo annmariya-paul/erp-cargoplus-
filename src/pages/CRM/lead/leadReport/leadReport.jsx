139a6db3b3dbebafb92de2376f8f8903967c3356
Ann Mariya paul <annmaria@reontel.com>
2022-10-19 17:40:00 +0530

    lead

---
+++ src/pages/CRM/lead/leadReport/leadReport.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import "./leadReport.styles.scss";
+import TableData from "../../../../components/table/table_data";
+
+ export default function LeadReport(){
+return(
+    <>
+
+
+    </>
+)
+ }

6b29b1695aa8af647717879b98d4c7d6aa1711d3
Ann Mariya paul <annmaria@reontel.com>
2022-10-26 10:31:24 +0530

    Attributes

--- src/App.js
+++ src/App.js
@@ -11,14 +11,10 @@ import TestPage from "./pages/testpage";
 import LeadReport from "./pages/CRM/lead/leadReport/leadReport";
 import Opportunitylist from "./pages/opportunity_ List/opportunitylist";
 import Categorylist from "./pages/category/viewCategory";
-<<<<<<< HEAD
 
-import BrandsList from "./pages/CRM/lead/brands/BrandsList";
-
-import OpportunityReport from "./pages/opportunityReport/OpportunityReport";
 import Attribute from "./pages/CRM/attributes/attributes";
 import Add_Attribute from "./pages/CRM/attributes/add_attribute";
-=======
+
 import BrandsList from "./pages/CRM/lead/brands/BrandsList";
 import OpportunityReport from "./pages/opportunityReport/OpportunityReport";
 
@@ -26,7 +22,6 @@ import Viewunit from "./pages/unit/ViewUnit";
 import BrandCreate from "./pages/CRM/lead/brands/BrandCreate";
 import Productlist from "./pages/CRM/lead/Product/Productlist";
 import ProductCreate from "./pages/CRM/lead/Product/ProductCreate";
->>>>>>> 1353d62335126fa06d072b9304075451d6df855b
 
 // import Lead from "./pages/lead/lead";
 
@@ -46,26 +41,18 @@ function App() {
             <Route path={ROUTES.OPPORTUNITY} element={<Opportunitylist />} />
             <Route path={ROUTES.CATEGORY_LIST} element={<Categorylist />} />
             <Route path={ROUTES.LEAD_REPORT} element={<LeadReport />} />
-<<<<<<< HEAD
             <Route path={ROUTES.BRANDS} element={<BrandsList />} />
             <Route path={ROUTES.ATTRIBUTES} element={<Attribute />} />
             <Route path={ROUTES.ADD_ATTRIBUTES} element={<Add_Attribute />} />
-=======
 
-            <Route path={ROUTES.BRANDS} element={<BrandsList />} />
-
->>>>>>> 1353d62335126fa06d072b9304075451d6df855b
             <Route
               path={ROUTES.OPPORTUNITY_REPORT}
               element={<OpportunityReport />}
             />
-<<<<<<< HEAD
-=======
             <Route path={ROUTES.UNIT_LIST} element={<Viewunit />} />
             <Route path={ROUTES.BRANDCREATE} element={<BrandCreate />} />
             <Route path={ROUTES.PRODUCT} element={<Productlist />} />
             <Route path={ROUTES.PRODUCTCREATE} element={<ProductCreate />} />
->>>>>>> 1353d62335126fa06d072b9304075451d6df855b
           </Route>
         </Routes>
       </BrowserRouter>
--- src/routes/index.js
+++ src/routes/index.js
@@ -11,10 +11,8 @@ const ROUTES = {
   CATEGORY_LIST: "/categorylist",
   LEAD_REPORT: "/leadReport",
   BRANDS: "/brands",
-<<<<<<< HEAD
   ATTRIBUTES: "/attributes",
   ADD_ATTRIBUTES: "/add_attribute",
-=======
 
   OPPORTUNITY_REPORT: "/opportunity-report",
 
@@ -22,7 +20,6 @@ const ROUTES = {
   BRANDCREATE: "/createbrand",
   PRODUCT: "/product",
   PRODUCTCREATE: "/create_product",
->>>>>>> 1353d62335126fa06d072b9304075451d6df855b
 };
 
 module.exports = { ROUTES };

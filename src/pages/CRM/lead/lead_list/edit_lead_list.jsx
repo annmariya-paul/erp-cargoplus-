ea9945155a1432d5e1d4ff07aaac54fbb2b21c0c
Ann Mariya paul <annmaria@reontel.com>
2022-11-29 12:44:44 +0530

    edit lead basic validation

--- src/pages/CRM/lead/lead_list/edit_lead_list.jsx
+++ src/pages/CRM/lead/lead_list/edit_lead_list.jsx
@@ -5,7 +5,7 @@ import { message } from "antd";
 // import "./lead.styles.scss";
 import { Link } from "react-router-dom";
 import Button from "../../../../components/button/button";
-import { Form } from "react-bootstrap";
+import { Form } from "antd";
 import { ROUTES } from "../../../../routes";
 import { AiOutlinePlus } from "react-icons/ai";
 import { useForm } from "react-hook-form";
@@ -19,6 +19,9 @@ import PublicFetch from "../../../../utils/PublicFetch";
 import { LeadStatus } from "../../../../utils/leadStatus";
 import { CRM_BASE_URL } from "../../../../api/bootapi";
 import Countrystate from "../location/countryselect";
+import InputType from "../../../../components/Input Type textbox/InputType";
+import TextArea from "../../../../components/ InputType TextArea/TextArea";
+import SelectBox from "../../../../components/Select Box/SelectBox";
 // import ErrorMsg from "../../components/errormessage";
 import Custom_model from "../../../../components/custom_modal/custom_model";
 
@@ -45,6 +48,7 @@ function LeadEdit() {
   const [leadStatus, setLeadStatus] = useState("");
   const [leadId, setLeadId] = useState();
 
+  const [addForm] = Form.useForm();
   const [error, setError] = useState(false);
   const toggleTab = (index) => {
     setToggleState(index);
@@ -210,173 +214,223 @@ function LeadEdit() {
                 }
               >
                 {/* <div className="col-12"> */}
-                <div className="row mb-2 justify-content-end">
-                  <div class="col">
+                <div className="row mb-2 ">
+                  <div class="col-md-7 col-sm-12">
                     <h5 class="lead_text">Lead Edit ({leadName})</h5>
                   </div>
-                  <div
-                    className="col-2 d-flex"
-                    style={{ justifyContent: "center" }}
-                  >
-                    <Button
-                      btnType="add_borderless"
-                      onClick={() => setModalOpportunity(true)}
-                    >
-                      <BsPlusCircleFill style={{ fontSize: "16px" }} /> Add
-                      Opportunity
-                    </Button>
-                    <AddOpportunity
-                      modalOpportunity={modalOpportunity}
-                      onCancel={() => setModalOpportunity(false)}
-                      style="width:1250px"
-                    />
-                  </div>
-                  <div
-                    className="col-2 d-flex"
-                    style={{ justifyContent: "center" }}
-                  >
-                    <Link to={`${ROUTES.OPPORTUNITY_LEAD}/${id}`}
-                    className="nav-link">
-                      <Button onClick={Submit} btnType="add_borderless">
-                        <BsPlusCircleFill style={{ fontSize: "16px" }} /> View
-                        Opportunity
-                      </Button>
-                    </Link>
-                  </div>
-                </div>
-                {/* </div> */}
-
-                <div className="row px-1">
-                  <div className="col-sm-4 pt-2">
-                    <Form.Group className="mb-2" controlId="lead_type">
-                      <Form.Label>Type</Form.Label>
-                      <Form.Select
-                        aria-label="lead_type"
-                        name="lead_type"
-                        value={leadType}
-                        onChange={(e) => setLeadType(e.target.value)}
+                  <div className="col-md-5 col-sm-12 ">
+                    <div className="d-flex justify-content-end">
+                      <div
+                        className="col-md-5 col-sm-6  d-flex justify-content-end"
+                        // style={{ justifyContent: "center" }}
                       >
-                        <option value="L" selected="selected">
-                          Lead
-                        </option>
-                        <option value="C">Customer</option>
-                      </Form.Select>
-                    </Form.Group>
-                  </div>
-                  <div className="col-sm-4 pt-2">
-                    <Form.Group className="mb-2" controlId="lead_customer_name">
-                      <Form.Label>Name</Form.Label>
-                      <Form.Control
-                        type="text"
-                        name="lead_customer_name"
-                        required
-                        value={leadName}
-                        onChange={(e) => setLeadName(e.target.value)}
-                      />
-                      {/* <Form.Control.Feedback type="invalid">
-                        name is not registered
-                      </Form.Control.Feedback> */}
-                    </Form.Group>
-                  </div>
-                  <div className="col-sm-4 pt-2">
-                    <Form.Group className="mb-2" controlId="lead_user_type">
-                      <Form.Label>User Type</Form.Label>
-                      <Form.Select
-                        aria-label="lead_user_type"
-                        name="lead_user_type"
-                        value={leadUsertype}
-                        onChange={(e) => setLeadUsertype(e.target.value)}
+                        <Button
+                          btnType="add_borderless"
+                          onClick={() => setModalOpportunity(true)}
+                        >
+                          <BsPlusCircleFill style={{ fontSize: "16px" }} /> Add
+                          Opportunity
+                        </Button>
+                        <AddOpportunity
+                          modalOpportunity={modalOpportunity}
+                          onCancel={() => setModalOpportunity(false)}
+                          style="width:1250px"
+                        />
+                      </div>
+                      <div
+                        className="col-md-6 col-sm-4  d-flex justify-content-end"
+                        // style={{ justifyContent: "center" }}
                       >
-                        <option value="O" selected>
-                          Organisation
-                        </option>
-                        <option value="I">Indivdual</option>
-                      </Form.Select>
-                    </Form.Group>
-                  </div>
-                  <div className="col-sm-4 pt-2">
-                    <Form.Group className="mb-2" controlId="lead_organization">
-                      <Form.Label>Organisation</Form.Label>
-                      <Form.Control
-                        type="text"
-                        name="lead_organization"
-                        value={leadOrganization}
-                        onChange={(e) => setLeadOrganization(e.target.value)}
-                      />
-                    </Form.Group>
-                  </div>
-                  <div className="col-sm-4 pt-2">
-                    <Form.Group className="mb-2" controlId="lead_source">
-                      <Form.Label>Source</Form.Label>
-                      <Form.Select
-                        aria-label="lead_source"
-                        name="lead_source"
-                        value={leadSource}
-                        onChange={(e) => setLeadSource(e.target.value)}
-                      >
-                        <option value="reference">Reference</option>
-                        <option value="direct visit">Direct Visit</option>
-                        <option value="online registration" selected>
-                          Online Registration
-                        </option>
-                      </Form.Select>
-                    </Form.Group>
-                  </div>
-                  <div className="col-12 mt-3">
-                    <FileUpload
-                      value={leadAttachment}
-                      onChange={(e) => setLeadAttachment(e.target.files[0])}
-                    />
-                  </div>
-                  <div className="col-sm-8 pt-3">
-                    <Form.Group className="mb-2" controlId="lead_description">
-                      <Form.Label>Description</Form.Label>
-                      <Form.Control
-                        as="textarea"
-                        rows={3}
-                        value={leadDescription}
-                        onChange={(e) => setLeadDescription(e.target.value)}
-                      />
-                    </Form.Group>
-                  </div>
-                  <div className="col-sm-4 pt-3">
-                    <div className="row">
-                      <div className="col-12">
-                        <Form.Group className="mb-2" controlId="lead_status">
-                          <Form.Label>Lead Status</Form.Label>
-                          <Form.Select
-                            aria-label="lead_status"
-                            name="lead_status"
-                            value={leadStatus}
-                            onChange={(e) => setLeadStatus(e.target.value)}
-                          >
-                            <option value="LE">Lead</option>
-                            <option value="OP">Opportunity</option>
-                            <option value="QU">Quotation</option>
-                            <option value="IN" selected>
-                              Interested
-                            </option>
-                            <option value="CO">Converted</option>
-                            <option value="LO">Lost</option>
-                            <option value="DN">DND</option>
-                          </Form.Select>
-                        </Form.Group>
+                        <Link
+                          to={`${ROUTES.OPPORTUNITY_LEAD}/${id}`}
+                          className="nav-link"
+                        >
+                          <Button onClick={Submit} btnType="add_borderless">
+                            <BsPlusCircleFill style={{ fontSize: "16px" }} />{" "}
+                            View Opportunity
+                          </Button>
+                        </Link>
                       </div>
                     </div>
                   </div>
-
-                  <div className="col pt-3">
-                    <Button type="submit" onClick={updateUser} btnType="save">
-                      Update
-                    </Button>
-                    <Custom_model
-                      size={`sm`}
-                      success
-                      show={modalShow}
-                      onHide={() => setModalShow(false)}
-                    />
-                  </div>
                 </div>
+
+                <Form
+                  form={addForm}
+                  onFinish={(values) => {
+                    console.log("values iss", values);
+                    updateUser();
+                  }}
+                  onFinishFailed={(error) => {
+                    console.log(error);
+                  }}
+                >
+                  <div className="row px-1">
+                    <div className="col-sm-4 pt-2">
+                      <label>Type</label>
+                      <Form.Item
+                        name="leadType"
+                        rules={[
+                          {
+                            required: true,
+                            message: "Please select a Type",
+                          },
+                        ]}
+                      >
+                        <SelectBox
+                          value={leadType}
+                          onChange={(e) => setLeadType(e.target.value)}
+                        >
+                          <option>Lead</option>
+                          <option>Customer</option>
+                        </SelectBox>
+                      </Form.Item>
+                    </div>
+                    <div className="col-sm-4 pt-2">
+                      <label>Name</label>
+                      <Form.Item
+                        name="leadName"
+                        rules={[
+                          {
+                            required: true,
+                            pattern: new RegExp("^[A-Za-z0-9 ]+$"),
+                            message: "Please enter a Valid Name",
+                          },
+                          {
+                            whitespace: true,
+                          },
+                          {
+                            min: 2,
+                            message: "Name must be atleast 2 characters",
+                          },
+                          {
+                            max: 100,
+                          },
+                        ]}
+                      >
+                        <InputType
+                          value={leadName}
+                          onChange={(e) => setLeadName(e.target.value)}
+                        />
+                      </Form.Item>
+                      {/* <Form.Group
+                        className="mb-2"
+                        controlId="lead_customer_name"
+                      >
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                          type="text"
+                          name="lead_customer_name"
+                          required
+                          value={leadName}
+                          onChange={(e) => setLeadName(e.target.value)}
+                        />
+
+                      </Form.Group> */}
+                    </div>
+                    <div className="col-sm-4 pt-2">
+                      {/* <Form.Group className="mb-2" controlId="lead_user_type">
+                        <Form.Label>User Type</Form.Label>
+                        <Form.Select
+                          aria-label="lead_user_type"
+                          name="lead_user_type"
+                          value={leadUsertype}
+                          onChange={(e) => setLeadUsertype(e.target.value)}
+                        >
+                          <option value="O" selected>
+                            Organisation
+                          </option>
+                          <option value="I">Indivdual</option>
+                        </Form.Select>
+                      </Form.Group> */}
+                    </div>
+                    <div className="col-sm-4 pt-2">
+                      {/* <Form.Group
+                        className="mb-2"
+                        controlId="lead_organization"
+                      >
+                        <Form.Label>Organisation</Form.Label>
+                        <Form.Control
+                          type="text"
+                          name="lead_organization"
+                          value={leadOrganization}
+                          onChange={(e) => setLeadOrganization(e.target.value)}
+                        />
+                      </Form.Group> */}
+                    </div>
+                    <div className="col-sm-4 pt-2">
+                      {/* <Form.Group className="mb-2" controlId="lead_source">
+                        <Form.Label>Source</Form.Label>
+                        <Form.Select
+                          aria-label="lead_source"
+                          name="lead_source"
+                          value={leadSource}
+                          onChange={(e) => setLeadSource(e.target.value)}
+                        >
+                          <option value="reference">Reference</option>
+                          <option value="direct visit">Direct Visit</option>
+                          <option value="online registration" selected>
+                            Online Registration
+                          </option>
+                        </Form.Select>
+                      </Form.Group> */}
+                    </div>
+                    <div className="col-12 mt-3">
+                      <FileUpload
+                        value={leadAttachment}
+                        onChange={(e) => setLeadAttachment(e.target.files[0])}
+                      />
+                    </div>
+                    <div className="col-sm-8 pt-3">
+                      {/* <Form.Group className="mb-2" controlId="lead_description">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control
+                          as="textarea"
+                          rows={3}
+                          value={leadDescription}
+                          onChange={(e) => setLeadDescription(e.target.value)}
+                        />
+                      </Form.Group> */}
+                    </div>
+                    <div className="col-sm-4 pt-3">
+                      <div className="row">
+                        <div className="col-12">
+                          {/* <Form.Group className="mb-2" controlId="lead_status">
+                            <Form.Label>Lead Status</Form.Label>
+                            <Form.Select
+                              aria-label="lead_status"
+                              name="lead_status"
+                              value={leadStatus}
+                              onChange={(e) => setLeadStatus(e.target.value)}
+                            >
+                              <option value="LE">Lead</option>
+                              <option value="OP">Opportunity</option>
+                              <option value="QU">Quotation</option>
+                              <option value="IN" selected>
+                                Interested
+                              </option>
+                              <option value="CO">Converted</option>
+                              <option value="LO">Lost</option>
+                              <option value="DN">DND</option>
+                            </Form.Select>
+                          </Form.Group> */}
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col pt-3">
+                      <Button type="submit" onClick={updateUser} btnType="save">
+                        Update
+                      </Button>
+                    </div>
+                  </div>
+                </Form>
+                <Custom_model
+                  size={`sm`}
+                  success
+                  show={modalShow}
+                  onHide={() => setModalShow(false)}
+                />
               </div>
               <div
                 className={

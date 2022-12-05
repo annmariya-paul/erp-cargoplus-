ca586f305ec1940ae98a890ecc735d81ba3efc95
ShahidaFiroz1 <shahida@reontel.com>
2022-12-05 10:37:45 +0530

    new

--- src/pages/CRM/Selling/Product/Productlist.jsx
+++ src/pages/CRM/Selling/Product/Productlist.jsx
@@ -102,7 +102,29 @@ function Productlist() {
       .then((res) => {
         if (res?.data?.success) {
           console.log("All products success::: ", res?.data?.data.products);
-          setProducts(res?.data?.data.products);
+          let tempArr = [];
+          let arr2 = []
+          res?.data?.data?.products.forEach((item,index)=> {
+            console.log("gdghhh",item.crm_v1_categories);
+
+            // item?.crm_v1_categories?.forEach((itm,indx)=>{
+            //   arr2.push(itm?.category_name)
+            // for (let i=0; i<item?.crm_v1_categories.length; i++){
+            //   console.log("jefjjjehdfhe");
+            //   arr2.push(item?.category_name)
+            // }
+            tempArr.push({
+              product_id:item?.product_id,
+              product_name: item?.product_name,
+              catgeory_name:item.crm_v1_categories?.category_name,
+              product_code:item?.product_code,
+              product_pic:item?.product_pic,
+
+            })
+          })
+
+console.log("hellooooo",tempArr);
+          setProducts(tempArr);
           
           // let samplearry = [];
           // res?.data?.data?.leads.forEach((item, index) => {
@@ -226,7 +248,7 @@ function Productlist() {
     },
     {
       title: "CATEGORY",
-      dataIndex: "product_category_id",
+      dataIndex: "catgeory_name",
       key: "CATEGORY",
       width: "14%",
       align: "center",

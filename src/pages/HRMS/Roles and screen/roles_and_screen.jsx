77de194ac118eb0b05ca91dd96209e1368731b1f
MIMI-CODR <athulbabu@reontel.com>
2023-01-02 11:07:26 +0530

    Role and assign permission page api integration

--- src/pages/HRMS/Roles and screen/roles_and_screen.jsx
+++ src/pages/HRMS/Roles and screen/roles_and_screen.jsx
@@ -1,14 +1,100 @@
 import { EditOutlined } from "@ant-design/icons";
 import { Form } from "antd";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../../../components/button/button";
+import Custom_model from "../../../components/custom_modal/custom_model";
 import InputType from "../../../components/Input Type textbox/InputType";
 import TableData from "../../../components/table/table_data";
+import PublicFetch from "../../../utils/PublicFetch";
 import "./roles_screen.scss";
 
 function Roles_and_Screen() {
+  const [addForm] = Form.useForm();
+  // const [editForm] = Form.useForm();
   const [isEditOn, setIsEditOn] = useState(false);
   const [isEditTwo, setIsEditTwo] = useState(false);
+  const [successModal, setSuccessModal] = useState(false);
+  const [getallRoles, setGetAllRoles] = useState();
+  const [role_id, setRole_id] = useState();
+
+  const close_modal = (mShow, time) => {
+    if (!mShow) {
+      setTimeout(() => {
+        setSuccessModal(false);
+      }, time);
+    }
+  };
+  const CreateRole = (data) => {
+    PublicFetch.post(
+      `${process.env.REACT_APP_BASE_URL}/permissions/roles`,
+      data
+    )
+      .then((res) => {
+        console.log("Response", res);
+        if (res.data.success) {
+          console.log("Success", res.data.data);
+          setSuccessModal(true);
+          close_modal(successModal, 1200);
+          getRoles();
+          addForm.resetFields();
+        }
+      })
+      .catch((err) => {
+        console.log("Error", err);
+      });
+  };
+
+  const getRoles = () => {
+    PublicFetch.get(`${process.env.REACT_APP_BASE_URL}/permissions/roles`)
+      .then((res) => {
+        console.log("Response", res);
+        if (res.data.success) {
+          console.log("Success of all Roles ", res.data.data);
+          setGetAllRoles(res.data.data);
+        }
+      })
+      .catch((err) => {
+        console.log("Error", err);
+      });
+  };
+
+  const handleEditClickRole = (data) => {
+    console.log("Data's to edit", data);
+    if (data) {
+      addForm.setFieldsValue({
+        id: data.id,
+        name: data.name,
+      });
+
+      setIsEditOn(true);
+      setRole_id(data.id);
+    }
+  };
+
+  const updateRoles = (data) => {
+    PublicFetch.patch(
+      `${process.env.REACT_APP_BASE_URL}/permissions/roles/${role_id}`,
+      data
+    )
+      .then((res) => {
+        console.log("Response", res);
+        if (res.data.success) {
+          console.log("Suucess in updating", res.data.data);
+          getRoles();
+          setSuccessModal(true);
+          close_modal(successModal, 1200);
+          setIsEditOn(false);
+          addForm.resetFields();
+        }
+      })
+      .catch((err) => {
+        console.log("Error", err);
+      });
+  };
+
+  useEffect(() => {
+    getRoles();
+  }, []);
 
   const Rolecolumns = [
     {
@@ -19,16 +105,16 @@ function Roles_and_Screen() {
     {
       title: "Name",
       dataIndex: "name",
-      key: "key",
+      key: "name",
     },
     {
       title: "Action",
       dataIndex: "action",
       key: "key",
-      render: () => {
+      render: (data, index) => {
         return (
           <div className="">
-            <div className="" onClick={() => setIsEditOn(true)}>
+            <div className="" onClick={() => handleEditClickRole(index)}>
               <EditOutlined />
             </div>
           </div>
@@ -46,7 +132,7 @@ function Roles_and_Screen() {
     {
       title: "Name",
       dataIndex: "name",
-      key: "key",
+      key: "name",
     },
     {
       title: "Action",
@@ -84,23 +170,65 @@ function Roles_and_Screen() {
             </div>
             {isEditOn ? (
               <div className="col-6 mt-2">
-                <Form>
-                  <Form.Item>
-                    <InputType value={"jjsddjdj"} />
+                <Form
+                  form={addForm}
+                  onFinish={(value) => {
+                    console.log("jdfhg", value);
+                    updateRoles(value);
+                  }}
+                >
+                  <Form.Item
+                    name="name"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Name is Required",
+                      },
+                      {
+                        min: 2,
+                        message: "Minimum 2 characters Required",
+                      },
+                    ]}
+                  >
+                    <InputType />
                   </Form.Item>
                   <div className="d-flex justify-content-center">
-                    <Button className="p-2 save_button_style">Save</Button>
+                    <Button type="submit" className="p-2 save_button_style">
+                      Save
+                    </Button>
                   </div>
                 </Form>
               </div>
             ) : (
               <div className="col-6 mt-2">
-                <Form>
-                  <Form.Item>
+                <Form
+                  id=""
+                  name="addForm"
+                  form={addForm}
+                  onFinish={(value) => {
+                    console.log("if successs of create", value);
+                    CreateRole(value);
+                  }}
+                >
+                  <Form.Item
+                    name="name"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Name is Required",
+                      },
+                      {
+                        min: 2,
+                        message: "Minimum 2 characters Required",
+                      },
+                    ]}
+                  >
                     <InputType />
                   </Form.Item>
                   <div className="d-flex justify-content-center">
-                    <Button className="p-2 save_button_style">Save</Button>
+                    <Button type="submit" className="p-2 save_button_style">
+                      Save
+                    </Button>
                   </div>
                 </Form>
               </div>
@@ -108,11 +236,11 @@ function Roles_and_Screen() {
 
             <div className="col-6">
               <div className="">
-                <TableData columns={Rolecolumns} data={data} />
+                <TableData columns={Rolecolumns} data={getallRoles} />
               </div>
             </div>
           </div>
-          <div className="row py-5">
+          {/* <div className="row py-5">
             <div className="col-12">
               <h5 style={{ color: "#0891d1" }}>Screens / Objects</h5>
             </div>
@@ -145,9 +273,10 @@ function Roles_and_Screen() {
                 <TableData columns={Screencolumns} data={data} />
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
+      <Custom_model show={successModal} success />
     </div>
   );
 }

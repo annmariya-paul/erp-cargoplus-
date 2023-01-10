7b6b9cc90fd7c67ec377a4924498db6ea11f470a
alansaji <alan@reontel.com>
2023-01-10 19:58:36 +0530

    permissions ui

---
+++ src/utils/permission_dummy.js
@@ -0,0 +1,526 @@
+export const PERMISSIONS = [
+  {
+    roleId: 1,
+    modules: [
+      {
+        name: "Hrms",
+        subModules: [
+          {
+            name: "Branches",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Departments",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Designation",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Employment type",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Employee",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Employee grade",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Role",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Assign permission",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Crm",
+        subModules: [
+          {
+            name: "Sale - Lead",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Sale - Opportunity",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Sale - Reports - Lead report",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Sale - Reports - Opportunity report",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Selling - Opportunity",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Selling - Brands",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Selling - Attributes",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Selling - Products",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Selling - Service",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Selling - Unit and measures",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "General - Countries",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Fms",
+        subModules: [
+          {
+            name: "Opportunity - opportunity assigns",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Opportunity - Track assignments",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Agent",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Quotations - Frignt types",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+          {
+            name: "Quotations - Quotation list",
+            permissions: [
+              {
+                name: "create",
+                enabled: true,
+              },
+              {
+                name: "update",
+                enabled: true,
+              },
+              {
+                name: "read",
+                enabled: true,
+              },
+              {
+                name: "delete",
+                enabled: true,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
